a1990a164a76646d3e036ffe93a141e159e85cfe
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-01-15 04:13:50 -0500

    Rewrite searchbar_focus.js. Convert to iife. Make buttons respond visually to being focused (not just hover).

--- js/searchbar_focus.js
+++ js/searchbar_focus.js
@@ -1,52 +1,35 @@
-bindSearchbarFocus();
 
-var searchbars; 
-function bindSearchbarFocus(searchbarElements) {
-    if (!searchbarElements) {
-        searchbars = document.getElementsByClassName('searchbar');
+var searchbarFocus = (function(){
+    let searchbars = document.getElementsByClassName('searchbar');
+    for (const searchbar of searchbars) {
+        searchbar.addEventListener('focusin', (e)=>searchbarFocus(e, searchbar, true), true);
+        searchbar.addEventListener('focusout', (e)=>searchbarFocus(e, searchbar, false), true);
+        searchbar.addEventListener("mouseenter", (e)=>searchbarFocus(e, searchbar, true, true), true);
+        searchbar.addEventListener("mouseleave", (e)=>searchbarFocus(e, searchbar, false, true), true);
     }
-    for (let i=0; i<searchbars.length; i++) {
-        searchbars[i].addEventListener('focusin', ()=>searchbarFocus('in'));
-        searchbars[i].addEventListener('focusout', ()=>searchbarFocus('out'));
-        searchbars[i].addEventListener("mouseenter", searchbarMouseEnter );
-        searchbars[i].addEventListener("mouseleave", searchbarMouseLeave );
-    }
-}
-
-function searchbarFocus(val) {
-    let parent = event.target.parentElement;
-    if (val == 'in') {
-        gainFocus(parent)
-    } else {
-        if (!parent.isHovered && (true || event.target.parentElement.querySelector(".searchField").value == "")) {
-            loseFocus(parent);
+    function searchbarFocus(e, searchbar, focused, mouse) {
+        e.stopPropagation();
+        if (mouse) {
+            if (focused)
+                searchbar.isHovered = true;
+            else
+                searchbar.isHovered = false;
         }
-
+        if (focused) {
+            gainFocus(searchbar)
+        } else {
+            loseFocus(searchbar);
+        }
     }
-    //parent.style.setProperty('opacity', newOpacity);
-}
-
-function searchbarMouseEnter(e) {
-    gainFocus(e.target);
-    e.target.isHovered = true;
-}
-function searchbarMouseLeave(e) {
-    loseFocus(e.target);
-    e.target.isHovered = false;
-
-}
-function gainFocus(elem) {
-    let newOpacity = 1;
-    gsap.to(elem, .15, { scale:1, opacity: newOpacity });
-    elem.style.setProperty("box-shadow", "var(--shadow)");
-}
-function loseFocus(elem) {
-    let newOpacity = 1;
-    if (!elem.contains(document.activeElement) && (true || elem.parentElement.querySelector(".searchField").value == "")) {
-        newOpacity = 0.8
-        //var default_scale = parseInt(parent.style.getPropertyValue('--default-scale'));
-        gsap.to(elem, .15, { scale: .99, opacity: newOpacity});
-        elem.style.setProperty("box-shadow", "none");
-        // elem.style.setProperty("border", "1px var(--color-element-1) solid");
+    function gainFocus(elem) {
+        gsap.to(elem, .15, { scale:1, opacity: 1 });
+        elem.style.setProperty("box-shadow", "var(--shadow)");
     }
-}
+    function loseFocus(elem) {
+        if (!elem.contains(document.activeElement) && !elem.isHovered) { //&& (true || elem.parentElement.querySelector(".searchField").value == "")
+            gsap.to(elem, .15, { scale: .99, opacity: 0.8});
+            elem.style.setProperty("box-shadow", "none");
+            // elem.style.setProperty("border", "1px var(--color-element-1) solid");
+        }
+    }
+})();
